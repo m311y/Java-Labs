26ddad0fc9017f5c2bbd42d9e32846feb84aab75
Melissa Hardwick <32114030+meowhard@users.noreply.github.com>
2021-04-13 10:32:56 -0400

    lol forgot chapter 2 lab 2

---
+++ Chapter2Lab2/src/Ch2Lab2.java
@@ -0,0 +1,18 @@
+
+public class Ch2Lab2 {
+
+	public static void main(String[] args) {
+		// TODO Find the area of a Parallelogram
+		
+		double area, base, height;
+		
+		base = 2.5;
+		height = 5.5;
+		
+		area = base * height;
+		
+		System.out.println("The Parallelogram:\nBase: " + base + " inches\nHeight: " + height + " inches\nArea: " + area + " inches");
+
+	}
+
+}
